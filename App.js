2dbc267ac3a7bf129109fd8cff38e8046678185e
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-10-09 10:03:50 +0700

    Add redux-thunk

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { StyleSheet, View, StatusBar } from 'react-native'
 import { Constants } from 'expo'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import reducer from './reducers'
 import { blue, lightGray, orange } from './utils/colors'
 import MainNavigator from './components/Base/Routes'
 import AppStatusBar from './components/Base/AppStatusBar'
 
+const store = createStore(reducer, applyMiddleware(thunk))
+
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(reducer)}>
+      <Provider store={store}>
         <View style={{ flex: 1 }}>
           <AppStatusBar backgroundColor={blue} barStyle="light-content" />
           <MainNavigator />
