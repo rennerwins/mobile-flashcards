366ba2c78aad88fb2aba839df64d6f97f575ff55
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-10-10 22:38:05 +0700

    Add 'correct' and 'incorrect' button in quiz view

--- components/Quiz/QuizView.js
+++ components/Quiz/QuizView.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import AppAndroidButton from '../Base/AppAndroidButton'
-import { blue, white } from '../../utils/colors'
+import { blue, white, red, green } from '../../utils/colors'
 
 class QuizView extends Component {
   state = {
-    current: 1
+    current: 1,
+    correct: 0
   }
 
-  nextQuestion = () => {
+  correctAnswered = () => {
+    this.setState(state => ({
+      current: state.current + 1,
+      correct: state.correct + 1
+    }))
+  }
+
+  incorrectAnswered = () => {
     this.setState(state => ({
       current: state.current + 1
     }))
@@ -16,24 +24,51 @@ class QuizView extends Component {
 
   render() {
     const { deck } = this.props.navigation.state.params
-    const { current } = this.state
-    console.log(current)
+    const { current, correct } = this.state
 
     return (
       <View style={styles.container}>
-        <Text>
+        <Text style={{ fontSize: 18 }}>
           {current} / {deck.questions.length}
         </Text>
 
-        <Text>{deck.questions[current - 1].question}</Text>
+        <View style={styles.quizWrapper}>
+          <Text style={styles.questionAndAnswer}>{deck.questions[current - 1].question}</Text>
+          <Text style={[styles.linkText, { color: red }]}>Answer</Text>
+          <Text>{correct}</Text>
+        </View>
 
-        <AppAndroidButton
-          press={this.nextQuestion}
-          backgroundColor={blue}
-          borderColor={blue}
-          color={white}
-          title="Submit"
-        />
+        <View style={styles.answerWrapper}>
+          {current !== deck.questions.length ? (
+            <View>
+              <AppAndroidButton
+                press={this.correctAnswered}
+                backgroundColor={green}
+                borderColor={green}
+                color={white}
+                title="Correct"
+              />
+
+              <AppAndroidButton
+                press={this.incorrectAnswered}
+                backgroundColor={red}
+                borderColor={red}
+                color={white}
+                title="Incorrect"
+              />
+            </View>
+          ) : (
+            <View>
+              <AppAndroidButton
+                press={this.incorrectAnswered}
+                backgroundColor={red}
+                borderColor={red}
+                color={white}
+                title="Show Result"
+              />
+            </View>
+          )}
+        </View>
       </View>
     )
   }
@@ -44,6 +79,25 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 20,
     paddingTop: 5
+  },
+  quizWrapper: {
+    flex: 0.7,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  questionAndAnswer: {
+    fontSize: 34,
+    marginBottom: 10,
+    textAlign: 'center'
+  },
+  linkText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: 200
+  },
+  answerWrapper: {
+    alignItems: 'center'
   }
 })
 
