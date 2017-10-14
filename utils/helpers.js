7c9da6dbb95a06ebf6c4a3ffd826ddeead141da7
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-10-14 11:05:38 +0700

    Change notification time to 20.00

--- utils/helpers.js
+++ utils/helpers.js
@@ -42,7 +42,7 @@ export function setLocalNotification() {
 
             let tomorrow = new Date()
             tomorrow.setDate(tomorrow.getDate() + 1)
-            tomorrow.setHours(9)
+            tomorrow.setHours(20)
             tomorrow.setMinutes(0)
 
             Notifications.scheduleLocalNotificationAsync(createNotification(), {
